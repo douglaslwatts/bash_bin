b36f5a07c8d893655d2d9a763dd955483dc3bd80
Lee Watts <lwatts@aquent.com>
2023-01-05 17:51:18 -0500

    Add check_repos.sh as a quick dirty check for git status of multiple projects.

---
+++ bin/check_repos.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# A quick dirty script to see if any local Git repos are outdated.
+
+usage() {
+    echo "Usage: $0 -d <project directory>"
+}
+
+unset -v project_dir
+
+while getopts "d:h" opt; do
+    case $opt in
+        d) project_dir=$OPTARG;;
+        h) usage $0
+           exit 0;;
+        \?) echo "Invalid option: -${OPTARG}" >&2
+            usage $0
+            exit 1;;
+   esac
+done
+
+if [ -z "$project_dir" ]; then
+    usage $0
+    exit 1
+fi
+
+if [ ! -d "$project_dir" ]; then
+    echo "Error! The directory given is not a directory!" >&2
+    usage $0
+    exit 1
+fi
+
+cd "$project_dir"
+
+for dir in **; do
+    if [ -d "$fdir" ]; then
+        cd $dir
+        if [ -d "./.git" ] &&
+           [ "$(git status | grep -o 'Your branch is up to date with')" == "" ]; then
+            echo -e "$dir is outdated and needs a pull or has uncomitted changes in branch.\n"
+            status="$(git status)"
+            echo -e "---------------------------------\n$status\n---------------------------------\n\n"
+        fi
+        cd ..
+    fi
+done
+
+exit 0
+
